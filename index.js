fef4cc020a6d9a6f39d83c08e5b5abd342477f2c
Roshan <108453526+roshanbachhav@users.noreply.github.com>
2022-10-11 09:29:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+const port = process.env.PORT || 3000;
+
+const apiData = require("./data.json");
+
+app.get("/",(req ,res) =>{
+    res.send();
+});
+
+app.get("/product", (req , res) =>{
+    res.send(apiData);
+})
+
+app.listen(port, () =>{
+
+});
